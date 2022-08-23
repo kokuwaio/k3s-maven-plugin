efb3aac389694f514413c618d45c8e564bc5865e
Stefan Wiedemann <wistefan@googlemail.com>
2022-08-23 14:59:37 +0200

    dont fail on successfull jobs

---
+++ src/it/cert-manager/src/test/java/io/kokuwa/maven/k3s/PodIT.java
@@ -0,0 +1,11 @@
+package io.kokuwa.maven.k3s;
+
+import org.junit.jupiter.api.Test;
+
+public class PodIT {
+
+	@Test
+	void test() {
+		// the test should only trigger the deployment and assert it does not fail because of the completed job.
+	}
+}
