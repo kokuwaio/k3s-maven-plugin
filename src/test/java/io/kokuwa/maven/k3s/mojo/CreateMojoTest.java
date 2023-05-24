32c059fc56832992b640ffdec3766cb02e8e95ca
Stephan Schnabel <stephan.schnabel@posteo.de>
2023-05-24 08:40:50 +0200

    Refactor unit tests (#210)
    - move test stuff to dedicated packages
    - use setters in mojos
    - add new test cases

--- src/test/java/io/kokuwa/maven/k3s/mojo/CreateMojoTest.java
+++ src/test/java/io/kokuwa/maven/k3s/mojo/CreateMojoTest.java
@@ -1,69 +1,78 @@
 package io.kokuwa.maven.k3s.mojo;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import io.kokuwa.maven.k3s.AbstractTest;
 import io.kokuwa.maven.k3s.AgentCacheMode;
+import io.kokuwa.maven.k3s.test.AbstractTest;
 
+/**
+ * Test for {@link CreateMojo}.
+ *
+ * @author stephan.schnabel@posteo.de
+ */
 @DisplayName("mojo: create")
 public class CreateMojoTest extends AbstractTest {
 
 	@DisplayName("with skip")
 	@Test
-	void withSkip(CreateMojo mojo) {
-		assertDoesNotThrow(() -> mojo.setSkip(true).execute());
+	void withSkip(CreateMojo createMojo) {
+		assertDoesNotThrow(() -> createMojo.setSkipCreate(false).setSkip(true).execute());
+		assertDoesNotThrow(() -> createMojo.setSkipCreate(true).setSkip(false).execute());
+		assertDoesNotThrow(() -> createMojo.setSkipCreate(true).setSkip(true).execute());
+		assertFalse(docker.getContainer().isPresent());
 	}
 
 	@DisplayName("with fail on existing container")
 	@Test
-	void withFailIfExists(CreateMojo mojo) {
-		assertDoesNotThrow(() -> mojo.execute());
-		var msg = assertThrows(MojoExecutionException.class, () -> mojo.setFailIfExists(true).execute()).getMessage();
-		assertAll("invalid message: " + msg,
-				() -> assertTrue(msg.startsWith("Container with id '")),
-				() -> assertTrue(msg.endsWith(
-						"' found. Please remove that container or set 'k3s.failIfExists' to false.")));
+	void withFailIfExists(CreateMojo createMojo) {
+		createMojo.setFailIfExists(true);
+		assertDoesNotThrow(createMojo::execute);
+		var message = "Container with id '" + docker.getContainer().orElseThrow().getId()
+				+ "' found. Please remove that container or set 'k3s.failIfExists' to false.";
+		assertThrowsExactly(MojoExecutionException.class, createMojo::execute, () -> message);
 	}
 
 	@DisplayName("with replace on existing container")
 	@Test
-	void withReplaceIfExists(CreateMojo mojo) {
-		assertDoesNotThrow(() -> mojo.execute());
-		var containerBefore = docker.getContainer();
-		assertDoesNotThrow(() -> mojo.setFailIfExists(false).setReplaceIfExists(true).execute());
-		var containerAfter = docker.getContainer();
-		assertNotEquals(containerBefore.get().getId(), containerAfter.get().getId(), "container was not replaced");
+	void withReplaceIfExists(CreateMojo createMojo) {
+		createMojo.setFailIfExists(false).setReplaceIfExists(true);
+		assertDoesNotThrow(createMojo::execute);
+		var containerBefore = docker.getContainer().orElseThrow();
+		assertDoesNotThrow(createMojo::execute);
+		var containerAfter = docker.getContainer().orElseThrow();
+		assertNotEquals(containerBefore.getId(), containerAfter.getId(), "container was not replaced");
 	}
 
 	@DisplayName("without fail on existing container")
 	@Test
-	void withoutFailIfExists(CreateMojo mojo) {
-		assertDoesNotThrow(() -> mojo.execute());
-		var containerBefore = docker.getContainer();
-		assertDoesNotThrow(() -> mojo.setFailIfExists(false).setReplaceIfExists(false).execute());
-		var containerAfter = docker.getContainer();
-		assertEquals(containerBefore.get().getId(), containerAfter.get().getId(), "container shouldn't be replaced");
+	void withoutFailIfExists(CreateMojo createMojo) {
+		createMojo.setFailIfExists(false).setReplaceIfExists(false);
+		assertDoesNotThrow(createMojo::execute);
+		var containerBefore = docker.getContainer().orElseThrow();
+		assertDoesNotThrow(createMojo::execute);
+		var containerAfter = docker.getContainer().orElseThrow();
+		assertEquals(containerBefore.getId(), containerAfter.getId(), "container shouldn't be replaced");
 	}
 
 	@DisplayName("with agent cache NONE")
 	@Test
-	void withCacheNone(CreateMojo mojo) {
-		assertDoesNotThrow(() -> mojo.setAgentCache(AgentCacheMode.NONE).execute());
+	void withCacheNone(CreateMojo createMojo) {
+		assertDoesNotThrow(() -> createMojo.setAgentCache(AgentCacheMode.NONE).execute());
 	}
 
 	@DisplayName("with agent cache VOLUME")
 	@Test
-	void withCacheVolume(CreateMojo mojo) {
-		assertDoesNotThrow(() -> mojo.setAgentCache(AgentCacheMode.VOLUME).execute());
+	void withCacheVolume(CreateMojo createMojo) {
+		assertDoesNotThrow(() -> createMojo.setAgentCache(AgentCacheMode.VOLUME).execute());
 		assertTrue(docker.isVolumePresent());
 		docker.getContainer().ifPresent(docker::removeContainer);
 		docker.removeVolume();
@@ -71,7 +80,7 @@ public class CreateMojoTest extends AbstractTest {
 
 	@DisplayName("with agent cache HOST")
 	@Test
-	void withCacheHost(CreateMojo mojo) {
-		assertDoesNotThrow(() -> mojo.setAgentCache(AgentCacheMode.HOST).execute());
+	void withCacheHost(CreateMojo createMojo) {
+		assertDoesNotThrow(() -> createMojo.setAgentCache(AgentCacheMode.HOST).execute());
 	}
 }
---
+++ src/test/java/io/kokuwa/maven/k3s/util/DockerTest.java
@@ -0,0 +1,68 @@
+package io.kokuwa.maven.k3s.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Duration;
+import java.util.function.BiConsumer;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.kokuwa.maven.k3s.test.AbstractTest;
+
+/**
+ * Test for {@link Docker}.
+ *
+ * @author stephan.schnabel@posteo.de
+ */
+@DisplayName("util: docker")
+public class DockerTest extends AbstractTest {
+
+	@DisplayName("image handling")
+	@Test
+	void image() throws MojoExecutionException {
+
+		assertFalse(docker.findImage(helloWorld()).isPresent(), "image found before testing");
+		docker.removeImage(helloWorld());
+
+		var callback = docker.pullImage(helloWorld());
+		Await.await("pull images").timeout(Duration.ofSeconds(300)).until(callback::isCompleted);
+		if (!callback.isSuccess()) {
+			throw new MojoExecutionException("Failed to pull image " + helloWorld());
+		}
+		assertTrue(docker.findImage(helloWorld()).isPresent(), "image missing after pulling");
+
+		docker.removeImage(helloWorld());
+		assertFalse(docker.findImage(helloWorld()).isPresent(), "image found after removing");
+	}
+
+	@DisplayName("normalizeImage()")
+	@Test
+	void normalizeImage() {
+
+		BiConsumer<String, String> assertImage = (e, i) -> assertEquals(e, docker.normalizeDockerImage(i), i);
+
+		assertImage.accept("docker.io/library/hello:latest", "hello");
+		assertImage.accept("docker.io/library/hello:latest", "hello:latest");
+		assertImage.accept("docker.io/library/hello:0.1.23", "hello:0.1.23");
+		assertImage.accept("docker.io/library/hello@sha256:XYZ", "hello@sha256:XYZ");
+
+		assertImage.accept("docker.io/hello/world:latest", "hello/world");
+		assertImage.accept("docker.io/hello/world:latest", "hello/world:latest");
+		assertImage.accept("docker.io/hello/world:0.1.23", "hello/world:0.1.23");
+		assertImage.accept("docker.io/hello/world@sha256:XYZ", "hello/world@sha256:XYZ");
+
+		assertImage.accept("docker.io/hello/world:latest", "docker.io/hello/world");
+		assertImage.accept("docker.io/hello/world:latest", "docker.io/hello/world:latest");
+		assertImage.accept("docker.io/hello/world:0.1.23", "docker.io/hello/world:0.1.23");
+		assertImage.accept("docker.io/hello/world@sha256:XYZ", "docker.io/hello/world@sha256:XYZ");
+
+		assertImage.accept("quay.io/hello/world:latest", "quay.io/hello/world");
+		assertImage.accept("quay.io/hello/world:latest", "quay.io/hello/world:latest");
+		assertImage.accept("quay.io/hello/world:0.1.23", "quay.io/hello/world:0.1.23");
+		assertImage.accept("quay.io/hello/world@sha256:XYZ", "quay.io/hello/world@sha256:XYZ");
+	}
+}
