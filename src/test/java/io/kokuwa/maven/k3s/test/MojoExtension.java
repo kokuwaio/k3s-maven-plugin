1101392c72c7e941176a22557d013d444aaac2c8
Stephan Schnabel <stephan@schnabel.org>
2025-05-12 14:39:12 +0200

    Use registry mirror for dev builds because if dockerhub limits

--- src/test/java/io/kokuwa/maven/k3s/test/MojoExtension.java
+++ src/test/java/io/kokuwa/maven/k3s/test/MojoExtension.java
@@ -22,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.kokuwa.maven.k3s.mojo.K3sMojo;
+import io.kokuwa.maven.k3s.mojo.RunMojo;
 import io.kokuwa.maven.k3s.util.Docker;
 
 /**
@@ -95,6 +96,12 @@ public class MojoExtension implements ParameterResolver, BeforeAllCallback {
 			}
 			mojo.setContainerName(containerName);
 			mojo.setVolumeName(volumeName);
+
+			if (mojo instanceof RunMojo) {
+				((RunMojo) mojo).setRegistries(new File("src/it/k3s-registries.yaml"));
+				((RunMojo) mojo).setDisableDefaultRegistryEndpoint(true);
+			}
+
 			return mojo;
 		} catch (ReflectiveOperationException e) {
 			throw new ParameterResolutionException("Failed to setup mojo " + descriptor + ".", e);
